0a0d6e39f41876646b74387e3b8ca67fb71608f4
Kenny Romero <sastoque.kenny@gmail.com>
2023-03-27 23:34:11 -0500

    Initial Commit

---
+++ Dogs Facts/Dogs_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dogs_FactsApp.swift
+//  Dogs Facts
+//
+//  Created by Kenny Yim on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Dogs_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
